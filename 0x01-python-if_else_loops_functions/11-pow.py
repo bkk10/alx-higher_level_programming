40ee770d8757bd40fd3ed4c817df942a53eebb83
rutob42 <rutob42@gmail.com>
2024-04-24 15:10:17 +0000

    Python - if/else, loops, functions Corrections

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,7 @@
-#!usr/bin/python3
-def pow(a,b):
-   if b==0:
-      return 1
-   else:
-      return(a**b)
+#!/usr/bin/python3
+def pow(a, b):
+     if b == 0:
+         return 1
+     else:
+         return(a ** b)
 
