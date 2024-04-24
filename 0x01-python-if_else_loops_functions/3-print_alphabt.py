8227eab801e2e37f5168f28a2b094bb342ea2b61
rutob42 <rutob42@gmail.com>
2024-04-24 15:22:26 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) != 'q' and chr(i) != 'e':
-      print('{:c}'.format(i), end=" ")
+for i in range(97, 123)
+    if(i==101) or (i==113)
+        continue
+    print(chr(i).format(), end ="")
