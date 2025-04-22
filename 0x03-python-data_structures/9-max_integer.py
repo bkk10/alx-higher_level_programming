ac4308bf877c33241e9b2a664ef2329b736f7e7d
bkk10 <brkkiprop@gmail.com>
2025-04-21 17:10:01 -0700

    Python- Data Structures: List, Tuples

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    largest = my_list[0]
+    if (len(my_list) == 0):
+        return None
+    for num in my_list:
+        if num > largest:
+            largest = num
+    return largest
